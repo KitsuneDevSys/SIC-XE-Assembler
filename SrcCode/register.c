b933d1106b300a0608aba7d24751cca3144094d1
SecantLDM <keyes.john07@gmail.com>
2021-11-27 19:08:27 -0500

    Register.c update

--- SrcCode/register.c
+++ SrcCode/register.c
@@ -9,31 +9,31 @@ int IsAValidRegister( char *Test )
 	}
 	else if((!(strcmp(Test,"X"))) == 1)
 	{
-		registervalue = 0x01;
+		registervalue = 0x10;
 	}
     else if((!(strcmp(Test,"B"))) == 1)
 	{
-		registervalue = 0x03;
+		registervalue = 0x30;
 	}
     else if((!(strcmp(Test,"S"))) == 1)
 	{
-		registervalue = 0x04;
+		registervalue = 0x40;
 	}
     else if((!(strcmp(Test,"T"))) == 1)
 	{
-		registervalue = 0x05;
+		registervalue = 0x50;
 	}
     else if((!(strcmp(Test,"F"))) == 1)
 	{
-		registervalue = 0x06;
+		registervalue = 0x60;
 	}
     else if((!(strcmp(Test,"PC"))) == 1)
 	{
-		registervalue = 0x08;
+		registervalue = 0x80;
 	}
     else if((!(strcmp(Test,"SW"))) == 1)
 	{
-		registervalue = 0x09;
+		registervalue = 0x90;
 	}
     return registervalue;
 }
